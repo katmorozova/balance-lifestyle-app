378146231dce5b7bc908d8324dc0f8aecfea2a9f
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-13 22:38:59 +0100

    Asignadas variables de activity_login.xml y inicializadas en metodo creado initViews()

--- app/src/main/java/com/example/balancelifestyle/LoginActivity.java
+++ app/src/main/java/com/example/balancelifestyle/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.balancelifestyle;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewResetPassword;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,13 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+    }
+
+    private void initViews(){
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        textViewResetPassword = findViewById(R.id.textViewResetPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
     }
 }
