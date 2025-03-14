1cb18666d39bb0341ae0915b4947e999123b1a6e
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-14 15:02:23 +0100

    Creado metodo que devuelve Intent para UserActivity

--- app/src/main/java/com/example/balancelifestyle/UserActivity.java
+++ app/src/main/java/com/example/balancelifestyle/UserActivity.java
@@ -1,5 +1,7 @@
 package com.example.balancelifestyle;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -20,5 +22,9 @@ public class UserActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public static Intent newIntent(Context context){
+        return new Intent(context, UserActivity.class);
     }
 }
