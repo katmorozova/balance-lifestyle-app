ffc59e599d7ed33e6908fb3d128efdcfedb713ac
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-14 22:36:16 +0100

    Añadido metodo signInWithEmailAndPassword en la variable auth dentro de login() y añadidos eventos de exito y fracaso

--- app/src/main/java/com/example/balancelifestyle/LoginViewModel.java
+++ app/src/main/java/com/example/balancelifestyle/LoginViewModel.java
@@ -1,7 +1,11 @@
 package com.example.balancelifestyle;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginViewModel extends ViewModel {
@@ -13,6 +17,17 @@ public class LoginViewModel extends ViewModel {
     }
 
     public void login(String email, String password){
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(
+                new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
 
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
     }
 }
