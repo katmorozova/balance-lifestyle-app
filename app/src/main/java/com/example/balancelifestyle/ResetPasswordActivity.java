2440c0b437ffaff7fd544944e41c40444e9c1d55
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-14 14:36:11 +0100

    Asignadas variables de activity_reset_password.xml y inicializadas en metodo creado initViews()

--- app/src/main/java/com/example/balancelifestyle/ResetPasswordActivity.java
+++ app/src/main/java/com/example/balancelifestyle/ResetPasswordActivity.java
@@ -1,6 +1,8 @@
 package com.example.balancelifestyle;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ResetPasswordActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private Button buttonResetPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,11 @@ public class ResetPasswordActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+    }
+
+    private void initViews(){
+        editTextEmail = findViewById(R.id.editTextEmail);
+        buttonResetPassword = findViewById(R.id.buttonResetPassword);
     }
 }
