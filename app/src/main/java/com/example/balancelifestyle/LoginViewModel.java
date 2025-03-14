7b59349e111a4ad58d36f3eec53aef4641a05d18
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-14 22:23:22 +0100

    Asignada variable de FirebaseAuth y inicializada en constructor de LoginViewModel

--- app/src/main/java/com/example/balancelifestyle/LoginViewModel.java
+++ app/src/main/java/com/example/balancelifestyle/LoginViewModel.java
@@ -2,9 +2,13 @@ package com.example.balancelifestyle;
 
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginViewModel extends ViewModel {
 
-    public LoginViewModel(){
+    private FirebaseAuth auth;
 
+    public LoginViewModel(){
+        auth = FirebaseAuth.getInstance();
     }
 }
