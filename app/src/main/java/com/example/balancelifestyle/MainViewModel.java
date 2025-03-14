26f071f2f7e89639c05d383bb10d24901fc027f1
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-15 00:12:14 +0100

    Añadida LiveData para comprobar si usuario autorizado y agregado getter para ella en MainViewModel

--- app/src/main/java/com/example/balancelifestyle/MainViewModel.java
+++ app/src/main/java/com/example/balancelifestyle/MainViewModel.java
@@ -1,14 +1,24 @@
 package com.example.balancelifestyle;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainViewModel extends ViewModel {
 
     private FirebaseAuth auth;
 
+    private  MutableLiveData<FirebaseUser> user = new MutableLiveData<>();
+
+
     public MainViewModel(){
         auth = FirebaseAuth.getInstance();
+    }
+
+    public LiveData<FirebaseUser> getUser() {
+        return user;
     }
 }
