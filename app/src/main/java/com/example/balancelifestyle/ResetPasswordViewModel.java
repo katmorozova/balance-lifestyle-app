60a3fa4016ff1fd2e5031802dfced227136312ba
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-17 09:41:26 +0100

    Asignamos variable para FirebaseAuth y su valor en ResetPasswordViewModel

--- app/src/main/java/com/example/balancelifestyle/ResetPasswordViewModel.java
+++ app/src/main/java/com/example/balancelifestyle/ResetPasswordViewModel.java
@@ -2,8 +2,11 @@ package com.example.balancelifestyle;
 
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ResetPasswordViewModel extends ViewModel {
 
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
 
     public ResetPasswordViewModel(){
     }
