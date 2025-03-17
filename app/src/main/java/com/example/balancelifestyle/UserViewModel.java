3712de0f49e5964ab5ea1d4b12a06778438c11c5
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-17 23:43:12 +0100

    Asignado variable para FirebaseAuth y asignado valor en constructor de UserViewModel

--- app/src/main/java/com/example/balancelifestyle/UserViewModel.java
+++ app/src/main/java/com/example/balancelifestyle/UserViewModel.java
@@ -2,9 +2,13 @@ package com.example.balancelifestyle;
 
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class UserViewModel extends ViewModel {
 
-    public UserViewModel(){
+    private FirebaseAuth auth;
 
+    public UserViewModel(){
+        auth = FirebaseAuth.getInstance();
     }
 }
