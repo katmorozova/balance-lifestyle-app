e6ba2aba27a5c5d85de6d6698e088d39977329eb
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-17 09:45:43 +0100

    Llamado metodo sendPasswordResetEmail  con parametro email para variable auth en metodo resetUserPassword()

--- app/src/main/java/com/example/balancelifestyle/ResetPasswordViewModel.java
+++ app/src/main/java/com/example/balancelifestyle/ResetPasswordViewModel.java
@@ -1,7 +1,10 @@
 package com.example.balancelifestyle;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ResetPasswordViewModel extends ViewModel {
@@ -12,6 +15,16 @@ public class ResetPasswordViewModel extends ViewModel {
     }
 
     public void resetUserPassword(String email){
+        auth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
 
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
     }
 }
