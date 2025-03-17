fc9ad920f2f845b5539ac8f92274691b5ef5527a
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-17 09:16:42 +0100

    Creado ResetPasswordViewModel y su constructor

---
+++ app/src/main/java/com/example/balancelifestyle/ResetPasswordViewModel.java
@@ -0,0 +1,10 @@
+package com.example.balancelifestyle;
+
+import androidx.lifecycle.ViewModel;
+
+public class ResetPasswordViewModel extends ViewModel {
+
+    public ResetPasswordViewModel(){
+
+    }
+}
