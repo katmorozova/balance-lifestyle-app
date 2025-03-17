ee9e459d4f8a05ed4f11116b422d02feff2b5a63
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-17 08:18:28 +0100

    LiveData para guardar mensaje de error y comprobar estado de usuario con getters añadidos

--- app/src/main/java/com/example/balancelifestyle/SignUpViewModel.java
+++ app/src/main/java/com/example/balancelifestyle/SignUpViewModel.java
@@ -1,14 +1,28 @@
 package com.example.balancelifestyle;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpViewModel extends ViewModel {
 
     private FirebaseAuth auth;
 
+    private MutableLiveData<String> error = new MutableLiveData<>();
+    private  MutableLiveData<FirebaseUser> user = new MutableLiveData<>();
+
     public SignUpViewModel(){
         auth = FirebaseAuth.getInstance();
+    }
+
+    public LiveData<String> getError() {
+        return error;
+    }
+
+    public LiveData<FirebaseUser> getUser() {
+        return user;
     }
 }
