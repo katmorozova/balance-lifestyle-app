904797b3233768b791f7ba48fb41d8c11f3c75dc
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-17 23:50:30 +0100

    creada LiveData para comprobar estado de usuario actual y añadido getter para ella en UserViewModel

--- app/src/main/java/com/example/balancelifestyle/UserViewModel.java
+++ app/src/main/java/com/example/balancelifestyle/UserViewModel.java
@@ -1,14 +1,22 @@
 package com.example.balancelifestyle;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class UserViewModel extends ViewModel {
 
     private FirebaseAuth auth;
+    private  MutableLiveData<FirebaseUser> user = new MutableLiveData<>();
 
     public UserViewModel(){
         auth = FirebaseAuth.getInstance();
+    }
+
+    public LiveData<FirebaseUser> getUser() {
+        return user;
     }
 }
