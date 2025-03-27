d5ad7fe60f8f81bc3b5a941ccb04147cd5fbbe49
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-27 23:41:31 +0100

    Creado metodo setUpClickListeners() en AddHabitActivity y dentro de este metodo añadido evento setOnClickListener para boton buttonSaveHabit

--- app/src/main/java/com/example/balancelifestyle/AddHabitActivity.java
+++ app/src/main/java/com/example/balancelifestyle/AddHabitActivity.java
@@ -1,6 +1,7 @@
 package com.example.balancelifestyle;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -35,6 +36,16 @@ public class AddHabitActivity extends AppCompatActivity {
             return insets;
         });
         initViews();
+        setUpClickListeners();
+    }
+
+    private void setUpClickListeners(){
+        buttonSaveHabit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     private void initViews(){
