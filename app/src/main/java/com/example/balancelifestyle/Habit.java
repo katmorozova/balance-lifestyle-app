e4055b62ba0cb5f973f34e1d3f61dc327ec71b5b
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-27 10:32:25 +0100

    Creado constructor Habit con parametros, las variables inicializadas dentro del mismo constructor

--- app/src/main/java/com/example/balancelifestyle/Habit.java
+++ app/src/main/java/com/example/balancelifestyle/Habit.java
@@ -6,5 +6,11 @@ public class Habit {
     private String text;
     private int typeOfHabit;
 
+    public Habit(int id, String text, int typeOfHabit){
+        this.id = id;
+        this.text = text;
+        this.typeOfHabit = typeOfHabit;
+    }
+
 
 }
