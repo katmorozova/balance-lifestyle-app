3fd24a1bd99a4a9fab97a818b3e2a31d511d84ea
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-27 16:01:25 +0100

    Asignada variable para TextView de habit_item y asignado su valor dentro del metodo showHabits()

--- app/src/main/java/com/example/balancelifestyle/HabitsActivity.java
+++ app/src/main/java/com/example/balancelifestyle/HabitsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -62,6 +63,7 @@ public class HabitsActivity extends AppCompatActivity {
                     linearLayoutHabits,
                     false
             );
+            TextView textViewHabit = view.findViewById(R.id.textViewHabit);
             linearLayoutHabits.addView(view);
         }
     }
