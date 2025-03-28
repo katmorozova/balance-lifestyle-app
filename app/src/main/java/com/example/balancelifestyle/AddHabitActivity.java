8cdb9dfd0a711be319b9705f4039593964b3cd01
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-03-28 08:39:27 +0100

    Creado metodo que devuelve Intent en AddHabitActivity

--- app/src/main/java/com/example/balancelifestyle/AddHabitActivity.java
+++ app/src/main/java/com/example/balancelifestyle/AddHabitActivity.java
@@ -1,5 +1,7 @@
 package com.example.balancelifestyle;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -84,5 +86,10 @@ public class AddHabitActivity extends AppCompatActivity {
             typeOfHabit = 6;
         }
         return typeOfHabit;
+    }
+
+    public static Intent newIntent(Context context){
+        Intent intent = new Intent(context, AddHabitActivity.class);
+        return intent;
     }
 }
