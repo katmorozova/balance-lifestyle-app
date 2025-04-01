23f019166b034b1e9ed3d178a3c032d0bd6cd225
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-04-01 11:30:52 +0200

    Asignada variable para OnHabitClickListener en HabitsAdapter class

--- app/src/main/java/com/example/balancelifestyle/HabitsAdapter.java
+++ app/src/main/java/com/example/balancelifestyle/HabitsAdapter.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class HabitsAdapter extends RecyclerView.Adapter<HabitsAdapter.HabitsViewHolder> {
 
     private ArrayList<Habit> habits = new ArrayList<>();
+    private OnHabitClickListener onHabitClickListener;
 
     public void setHabits(ArrayList<Habit> habits) {
         this.habits = habits;
@@ -87,7 +88,7 @@ public class HabitsAdapter extends RecyclerView.Adapter<HabitsAdapter.HabitsView
 
     }
 
-    interface onHabitClickListener {
+    public interface OnHabitClickListener {
         void onHabitClick(Habit habit);
     }
 }
