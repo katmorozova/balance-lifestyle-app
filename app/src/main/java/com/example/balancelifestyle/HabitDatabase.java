5c395319369f9fcd319edc5a32f9fd525d6c4cf1
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-04-01 22:55:53 +0200

    realizada logica de la clase HabitDatabase

--- app/src/main/java/com/example/balancelifestyle/HabitDatabase.java
+++ app/src/main/java/com/example/balancelifestyle/HabitDatabase.java
@@ -1,10 +1,26 @@
 package com.example.balancelifestyle;
 
+import android.app.Application;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Habit.class}, version = 1)
 public abstract class HabitDatabase extends RoomDatabase {
 
+    private static HabitDatabase instance = null;
+    private static final String DB_NAME = "habits.db";
 
+    public static HabitDatabase getInstance(Application application) {
+        if(instance == null){
+            //crear ejemplar base de datos:
+            instance = Room.databaseBuilder(
+                    application,
+                    HabitDatabase.class,
+                    DB_NAME
+            ).build();
+        }
+        return instance;
+    }
 }
