f748521487f46b1b0a30f36e450c4a6ebc764eae
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-04-01 09:31:52 +0200

    Redifinidos los metodos en HabitsAdapter

--- app/src/main/java/com/example/balancelifestyle/HabitsAdapter.java
+++ app/src/main/java/com/example/balancelifestyle/HabitsAdapter.java
@@ -1,7 +1,25 @@
 package com.example.balancelifestyle;
 
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class HabitsAdapter extends RecyclerView.Adapter<> {
 
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
