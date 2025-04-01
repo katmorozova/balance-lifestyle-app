526772c05cdf028f9a14688985ed3737da9a5ef0
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-04-01 22:35:57 +0200

    Cambios en HabitsActivity agregados. Añadidas dependencias de Room en la app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,9 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.firebase:firebase-auth")
 
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/balancelifestyle/HabitsActivity.java
+++ app/src/main/java/com/example/balancelifestyle/HabitsActivity.java
@@ -4,25 +4,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+
 
 public class HabitsActivity extends AppCompatActivity {
 
