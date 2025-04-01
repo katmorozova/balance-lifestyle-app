d1dbc1a26d94e54b70d70fffe836856976aaebec
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-04-01 23:05:03 +0200

    Añadidos metodos en interface HabitsDao

--- app/src/main/java/com/example/balancelifestyle/HabitsDao.java
+++ app/src/main/java/com/example/balancelifestyle/HabitsDao.java
@@ -3,6 +3,14 @@ package com.example.balancelifestyle;
 
 import androidx.room.Dao;
 
+import java.util.ArrayList;
+
 @Dao
 public interface HabitsDao {
+
+    ArrayList<Habit> getHabits(); //recibe habito
+
+    void add(Habit habit);//agrega habito dentro de DB
+
+    void remove(int id);//Elimina habito por su id
 }
