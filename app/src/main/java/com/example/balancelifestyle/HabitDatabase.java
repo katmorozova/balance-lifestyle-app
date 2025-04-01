33c3defdcce7258fa59bdca19beda860c8a16807
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-04-01 22:48:51 +0200

    Añadido annotacion @Database() para clase HabitDatabase

--- app/src/main/java/com/example/balancelifestyle/HabitDatabase.java
+++ app/src/main/java/com/example/balancelifestyle/HabitDatabase.java
@@ -1,7 +1,9 @@
 package com.example.balancelifestyle;
 
+import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {Habit.class}, version = 1)
 public abstract class HabitDatabase extends RoomDatabase {
 
 
