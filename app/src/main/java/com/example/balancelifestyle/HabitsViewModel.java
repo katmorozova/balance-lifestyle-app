835a6d96f9ab1937fdd7dad592d8a811c9e04efc
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-04-02 09:44:46 +0200

    Creado HabitsViewModel con su constructor

---
+++ app/src/main/java/com/example/balancelifestyle/HabitsViewModel.java
@@ -0,0 +1,13 @@
+package com.example.balancelifestyle;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class HabitsViewModel extends AndroidViewModel {
+
+    public HabitsViewModel(@NonNull Application application){
+        super(application);
+    }
+}
