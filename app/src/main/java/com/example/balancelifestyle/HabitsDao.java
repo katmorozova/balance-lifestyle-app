549c2de94f4938e0b868d69e52b672218c921944
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-04-02 11:24:52 +0200

    Añadidos cambios en HabitsDao

--- app/src/main/java/com/example/balancelifestyle/HabitsDao.java
+++ app/src/main/java/com/example/balancelifestyle/HabitsDao.java
@@ -8,6 +8,8 @@ import androidx.room.Query;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.core.Completable;
+
 
 @Dao
 public interface HabitsDao {
@@ -16,7 +18,7 @@ public interface HabitsDao {
     LiveData<List<Habit>> getHabits(); //recibe habito
 
     @Insert
-    void add(Habit habit);//agrega habito dentro de DB
+    Completable add(Habit habit);//agrega habito dentro de DB
 
     @Query("DELETE FROM habits WHERE id = :id")
     void remove(int id);//Elimina habito por su id
