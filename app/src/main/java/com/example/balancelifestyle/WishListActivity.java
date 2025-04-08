c2b539dc8e30289bf72a3f5ba93263783bbf7128
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-04-08 09:03:28 +0200

    Asignadas variables para ImageViewButtons y RecyclerViews. Creado metodo initViews() en WishListActivity. Dentro de este metodo asignado valor para variables de ImageViewButtons y RecyclerViews

--- app/src/main/java/com/example/balancelifestyle/WishListActivity.java
+++ app/src/main/java/com/example/balancelifestyle/WishListActivity.java
@@ -1,14 +1,23 @@
 package com.example.balancelifestyle;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class WishListActivity extends AppCompatActivity {
+
+    private ImageButton imageButtonProjects;
+    private ImageButton imageButtonBooks;
+    private ImageButton imageButtonFilms;
+    private RecyclerView recyclerViewProjects;
+    private RecyclerView recyclerViewBooks;
+    private RecyclerView recyclerViewFilms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,15 @@ public class WishListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+    }
+
+    public void initViews(){
+        imageButtonProjects = findViewById(R.id.imageButtonProjects);
+        imageButtonBooks = findViewById(R.id.imageButtonBooks);
+        imageButtonFilms = findViewById(R.id.imageButtonFilms);
+        recyclerViewProjects = findViewById(R.id.recyclerViewProjects);
+        recyclerViewBooks = findViewById(R.id.recyclerViewBooks);
+        recyclerViewFilms = findViewById(R.id.recyclerViewFilms);
     }
 }
