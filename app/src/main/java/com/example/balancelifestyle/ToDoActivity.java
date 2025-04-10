94883d3ababf409b9a185541154bd976f191a314
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-04-10 22:48:18 +0200

    Asignadas variables para RecyclerView y FloatingActionButton en ToDoActivity. Creado metodo initViews() dentro de este metodo asignados valores para  RecyclerView y FloatingActionButton

--- app/src/main/java/com/example/balancelifestyle/ToDoActivity.java
+++ app/src/main/java/com/example/balancelifestyle/ToDoActivity.java
@@ -7,8 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ToDoActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewToDo;
+    private FloatingActionButton buttonAddNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,12 @@ public class ToDoActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+    }
+
+
+    private void initViews(){
+        recyclerViewToDo = findViewById(R.id.recyclerViewToDo);
+        buttonAddNote = findViewById(R.id.buttonAddNote);
     }
 }
