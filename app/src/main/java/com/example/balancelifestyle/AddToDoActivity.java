4268153723792c9af0fc4922d76cd4b79661ea8c
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-04-23 23:18:22 +0200

    Añadidos cambios en AddToDoActivity: metodos getTypeOfToDoList(), setUpClickListeners() y saveToDoList() en AddToDoActiivty

--- app/src/main/java/com/example/balancelifestyle/AddToDoActivity.java
+++ app/src/main/java/com/example/balancelifestyle/AddToDoActivity.java
@@ -3,6 +3,7 @@ package com.example.balancelifestyle;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -12,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.balancelifestyle.database.ToDoList;
 
 public class AddToDoActivity extends AppCompatActivity {
 
@@ -32,6 +35,7 @@ public class AddToDoActivity extends AppCompatActivity {
             return insets;
         });
         initViews();
+        setUpClickListeners();
     }
 
     private void initViews(){
@@ -39,10 +43,41 @@ public class AddToDoActivity extends AppCompatActivity {
         radioButtonHighPriority = findViewById(R.id.radioButtonHighPriority);
         radioButtonMediumPriority = findViewById(R.id.radioButtonMediumPriority);
         radioButtonLowPriority = findViewById(R.id.radioButtonLowPriority);
-        buttonSaveNote = findViewById(R.id.buttonAddNote);
+        buttonSaveNote = findViewById(R.id.buttonSaveNote);
     }
 
     public static Intent newIntent(Context context){
         return new Intent(context, AddToDoActivity.class);
     }
+
+    private int getTypeOfToDoList(){
+        int typeOfList = -1;
+        if(radioButtonHighPriority.isChecked()){
+            typeOfList = 0;
+        }else if(radioButtonMediumPriority.isChecked()){
+            typeOfList = 1;
+        }else if(radioButtonLowPriority.isChecked()) {
+            typeOfList = 2;
+        }
+        return typeOfList;
+    }
+
+    private void setUpClickListeners(){
+        buttonSaveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveToDoList();
+                Intent intent = ToDoActivity.newIntent(AddToDoActivity.this);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void saveToDoList(){
+        String text = editTextAddNote.getText().toString().trim();
+        int typeOfList = getTypeOfToDoList();
+        ToDoList toDoList = new ToDoList(0, text, typeOfList);
+    }
+
 }
