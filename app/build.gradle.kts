ef774fce777cac015a0b5e4ec6944e0a5feb0e36
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-04-24 21:47:47 +0200

    Renovadas dependencias de Room en gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ dependencies {
 
     implementation("com.google.firebase:firebase-auth")
 
-    implementation("androidx.room:room-runtime:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-runtime:2.7.1")
+    annotationProcessor("androidx.room:room-compiler:2.7.1")
 
     implementation ("io.reactivex.rxjava3:rxandroid:3.0.2")
     implementation ("io.reactivex.rxjava3:rxjava:3.1.5")
