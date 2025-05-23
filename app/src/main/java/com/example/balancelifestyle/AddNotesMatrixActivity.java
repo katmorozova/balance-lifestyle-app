ad801c51c50e8747c1a25027c2b51d2cc4caef11
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-23 08:33:10 +0200

    Asignadas variables de diseño y su valor en AddNotesMatrixActivity

--- app/src/main/java/com/example/balancelifestyle/AddNotesMatrixActivity.java
+++ app/src/main/java/com/example/balancelifestyle/AddNotesMatrixActivity.java
@@ -1,6 +1,9 @@
 package com.example.balancelifestyle;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AddNotesMatrixActivity extends AppCompatActivity {
+
+    private EditText editTextAddNoteMatrix;
+    private RadioButton radioButtonDoNow;
+    private RadioButton radioButtonPlaning;
+    private RadioButton radioButtonDelegate;
+    private RadioButton radioButtonDelete;
+    private Button buttonAddNoteMatrix;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,15 @@ public class AddNotesMatrixActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+    }
+
+    private void initViews(){
+        editTextAddNoteMatrix = findViewById(R.id.editTextAddNoteMatrix);
+        radioButtonDoNow = findViewById(R.id.radioButtonDoNow);
+        radioButtonPlaning = findViewById(R.id.radioButtonPlaning);
+        radioButtonDelegate = findViewById(R.id.radioButtonDelegate);
+        radioButtonDelete = findViewById(R.id.radioButtonDelete);
+        buttonAddNoteMatrix = findViewById(R.id.buttonAddNoteMatrix);
     }
 }
