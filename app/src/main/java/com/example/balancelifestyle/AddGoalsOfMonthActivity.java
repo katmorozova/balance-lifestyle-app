6becb8b19ef61cc0fc2057b6b52ee215c4c5997a
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-02 23:42:57 +0200

    Cambios en activity_add_goals_of_month.xml añadidos. Añadidas variables y su valor de diseño en AddGoalsOfMonthActivity

--- app/src/main/java/com/example/balancelifestyle/AddGoalsOfMonthActivity.java
+++ app/src/main/java/com/example/balancelifestyle/AddGoalsOfMonthActivity.java
@@ -1,6 +1,8 @@
 package com.example.balancelifestyle;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AddGoalsOfMonthActivity extends AppCompatActivity {
+
+    private EditText editTextAddGoals;
+    private EditText editTextAddDate;
+    private EditText editTextAddProposite;
+    private EditText editTextAddRecompence;
+    private EditText edittextAddAction0;
+    private EditText edittextAddAction1;
+    private EditText edittextAddAction2;
+    private EditText edittextAddAction3;
+    private EditText edittextAddAction4;
+    private Button buttonSaveGoalsMonth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,20 @@ public class AddGoalsOfMonthActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+    }
+
+
+    private void initViews(){
+        editTextAddGoals = findViewById(R.id.editTextAddGoals);
+        editTextAddDate = findViewById(R.id.editTextAddDate);
+        editTextAddProposite = findViewById(R.id.editTextAddProposite);
+        editTextAddRecompence = findViewById(R.id.editTextAddRecompence);
+        edittextAddAction0 = findViewById(R.id.edittextAddAction0);
+        edittextAddAction1 = findViewById(R.id.edittextAddAction1);
+        edittextAddAction2 = findViewById(R.id.edittextAddAction2);
+        edittextAddAction3 = findViewById(R.id.edittextAddAction3);
+        edittextAddAction4 = findViewById(R.id.edittextAddAction4);
+        buttonSaveGoalsMonth = findViewById(R.id.buttonGoalsOfMonth);
     }
 }
